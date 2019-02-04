d7baa89c96cf2d0409702935429ae16747650142
Cenk Senyuz <cenksenyuz@gmail.com>
2019-02-04 18:18:12 +0300

    Add  gitignore  , setup project files

---
+++ 100DaysOfSwift/day1.playground/Contents.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+// Variables
+var str = "Hello, playground"
+str = "Goodbye"
+str += ", Cenk!"
+
+var adress = "321 Ataköy"
+adress = "321 Ataköy/ Istanbul"
+var user = "Cenk"
+var greeting = "Hello, "
+print(greeting+user)
+
+// Strings and Integers
+var age:Int = 39
+var population  = 8_000_000
+
+// Multi-line Strings
+var str1 = """
+This goes
+over
+multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+// Doubles and Booleans
+var pi = 3.141
+var awesome = true
+
+// String Interpolation
+var score = 90
+var text = "Your score was \(score)"
+var result = "The test results are here: \(text)"
+
+// Constants
+let taylor = "swift"
+
+// Type annotations
+let album : String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks:Bool = true
+
+// Simple Test Summary
+
+
+
+
+
